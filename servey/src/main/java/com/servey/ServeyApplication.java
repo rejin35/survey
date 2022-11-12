743cd5743a0816942db2880270b96bca7ce9f589
Hp <Hp@DESKTOP-U6KNCOL>
2022-11-12 11:14:17 +0530

    initial commit

---
+++ servey/src/main/java/com/servey/ServeyApplication.java
@@ -0,0 +1,13 @@
+package com.servey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeyApplication.class, args);
+	}
+
+}
---
+++ servey/src/test/java/com/servey/ServeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
